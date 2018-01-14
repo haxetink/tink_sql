a008ec2bef806311efd16d42b093e20b92a7bcbf
Ben Merckx <ben@codeurs.be>
2018-01-14 16:28:24 +0100

    Update travis / add fixture

---
+++ tests/fixture/init.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS test;
+CREATE DATABASE test;
+CREATE TABLE `test`.`Schema` ( 
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+  `text` INT(100) NOT NULL, 
+  `number` INT(11) NULL, 
+  `boolean` TINYINT(1) NOT NULL, 
+  `toremove` VARCHAR(1) NOT NULL
+);
+ALTER TABLE `test`.`Schema` ADD UNIQUE (`number`);
