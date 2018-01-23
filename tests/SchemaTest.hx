3c36171cf3ce92fff482951cbac8fce370d55e0e
Ben Merckx <ben@codeurs.be>
2018-01-23 19:12:21 +0100

    Remove debug info

--- tests/SchemaTest.hx
+++ tests/SchemaTest.hx
@@ -13,7 +13,6 @@ class SchemaTest extends TestWithDb {
 		loadFixture('schema_$version');
 		return db.Schema.diffSchema()
 			.next(function (changes) {
-				trace(changes);
 				inspect(changes);
 				return changes;
 			})
@@ -25,7 +24,7 @@ class SchemaTest extends TestWithDb {
 				return asserts.done();
 			});
 	}
-	
+
 	public function diffIdentical()
 		return check(asserts, 'identical', function(changes) {
 			asserts.assert(changes.length == 0);
