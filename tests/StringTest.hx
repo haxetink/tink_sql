159ff8003ecd4e6025052af8ae3f74a98d99c3a0
Jason O'Neil <jason.oneil@gmail.com>
2018-01-25 21:47:16 +0800

    Use spaces in StringTest.hx

--- tests/StringTest.hx
+++ tests/StringTest.hx
@@ -12,22 +12,22 @@ using tink.CoreApi;
 @:allow(tink.unit)
 class StringTest extends TestWithDb {
 
-	@:before
-	public function createTable() {
-		return db.StringTypes.create();
-	}
+  @:before
+  public function createTable() {
+    return db.StringTypes.create();
+  }
 
-	@:after
-	public function dropTable() {
-		return db.StringTypes.drop();
-	}
+  @:after
+  public function dropTable() {
+    return db.StringTypes.drop();
+  }
 
-	public function insert() {
-		var mydate = new Date(2000, 0, 1, 0, 0, 0);
+  public function insert() {
+    var mydate = new Date(2000, 0, 1, 0, 0, 0);
     function generateString(length) {
       return StringTools.lpad("", ".", length);
     }
-		var future = db.StringTypes.insertOne({
+    var future = db.StringTypes.insertOne({
       id: null,
       text10: generateString(10),
       text255: generateString(255),
@@ -38,9 +38,9 @@ class StringTest extends TestWithDb {
       textDefault: generateString(65535),
       textMedium: generateString(70000),
       textLong: generateString(80000),
-		})
-			.next(function(id:Int) return db.StringTypes.first())
-			.next(function(row:StringTypes) {
+    })
+      .next(function(id:Int) return db.StringTypes.first())
+      .next(function(row:StringTypes) {
         asserts.assert(row.text10 == generateString(10));
         asserts.assert(row.text255 == generateString(255));
         asserts.assert(row.text999 == generateString(999));
@@ -50,14 +50,14 @@ class StringTest extends TestWithDb {
         asserts.assert(row.textMedium == generateString(70000));
         asserts.assert(row.textLong == generateString(80000));
 
-				return Noise;
-			});
+        return Noise;
+      });
 
-		future.handle(function(o) switch o {
-			case Success(_): asserts.done();
-			case Failure(e): asserts.fail(e, e.pos);
-		});
+    future.handle(function(o) switch o {
+      case Success(_): asserts.done();
+      case Failure(e): asserts.fail(e, e.pos);
+    });
 
-		return asserts;
-	}
+    return asserts;
+  }
 }
