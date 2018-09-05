677fd8d98f7c493ff2d8d9eb06f4c3ea8100d902
Kevin Leung <kevinresol@gmail.com>
2018-09-05 16:01:32 +0800

    Test procedure.first()

--- tests/ProcedureTest.hx
+++ tests/ProcedureTest.hx
@@ -9,8 +9,21 @@ class ProcedureTest extends TestWithDb {
     loadFixture('procedure');
     db.func.call(1).all().handle(function(o) switch o {
       case Success(result): 
+        asserts.assert(result.length == 2);
         asserts.assert(result[0].x == 1);
         asserts.assert(result[1].x == 2);
+        asserts.done();
+      case Failure(e):
+        asserts.fail(e);
+    });
+    return asserts;
+  }
+  
+  public function limit() {
+    loadFixture('procedure');
+    db.func.call(1).first().handle(function(o) switch o {
+      case Success(result): 
+        asserts.assert(result.x == 1);
         asserts.done();
       case Failure(e):
         asserts.fail(e);
