68d571bf716a1c7fc982f08a807ef8d15c3a579b
Ben Merckx <ben@codeurs.be>
2019-12-28 16:52:35 +0100

    Change limit of intiterator to function as from ... to, fixes #85

--- src/tink/sql/Limit.hx
+++ src/tink/sql/Limit.hx
@@ -9,7 +9,7 @@ private typedef LimitData = {
 abstract Limit(LimitData) from LimitData to LimitData {
   
   @:from static function ofIter(i:IntIterator):Limit
-    return @:privateAccess { limit: i.min, offset: i.max };
+    return @:privateAccess { limit: i.max - i.min, offset: i.min };
   
   @:from static function ofInt(i:Int):Limit
     return { limit: i, offset: 0 };
