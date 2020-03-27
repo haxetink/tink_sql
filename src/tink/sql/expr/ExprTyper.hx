7cd21be87a5af93f008ccaad36c62e8af4fe43ba
grepsuzette <grepsuzette@magazi.ne>
2020-03-27 13:47:34 +0800

    Add a TRIM function to tink.sql.expr.Functions. Mainly to allow hacking an Int type to a String
    A scenario where this gets useful is when we need to map an Int column to a String in a `.next()`.
    E.g.
    
    ```haxe
    // Without the TRIM:
    db.Foo
      .select( {
         id    : Foo.id,
         b     : Foo.b,
         c     : Foo.c,
         d     : Foo.d,
         e     : Foo.e,
         f     : Foo.f
       })
       .all()
       .next( o -> {
           id: Std.string(id).map(s -> someTransformation(s)),
           name: name,
           b: b,
           c: c,
           d: d,
           e: e,
           f: f
        });
    
    // Above is very tedious,
    // By contrast if we had a TRIM function:
    db.Foo
      .select( {
         ref   : Foo.id,
         b     : Foo.b,
         c     : Foo.c,
         d     : Foo.d,
         e     : Foo.e,
         f     : Foo.f
       })
       .all()
       .next( o -> { o.ref = someTransformation(o.ref); o; });
    
    // And no creation of another object, because the Promised object
    already types ref as a String.
    
    -------------------------------------------------------
    
    Maybe TRIM is a weird choice here, but I thought having a STRING methods
    simply returning an Expr<String> would be more confusing (because it is
    not a SQL function).
    
    Tell me if you think there is a better solution :)

--- src/tink/sql/expr/ExprTyper.hx
+++ src/tink/sql/expr/ExprTyper.hx
@@ -92,6 +92,7 @@ class ExprTyper {
       case EUnOp(Not | IsNull, _, _): Some(ValueType.VBool);
       case EUnOp((_: UnOp<Dynamic, Dynamic>) => Neg, _, _): Some(ValueType.VBool);
       case ECall('COUNT', _): Some(ValueType.VInt);
+      case ECall('TRIM', _): Some(ValueType.VString);
       case ECall('ST_Distance_Sphere', _): Some(ValueType.VFloat);
       case ECall('IF', [_, ifTrue, _]): type(ifTrue);
       case ECall(_, _): Some(ValueType.VBool);
--- src/tink/sql/expr/Functions.hx
+++ src/tink/sql/expr/Functions.hx
@@ -17,6 +17,9 @@ class Functions {
   public static function min<D,O>(e:Field<D,O>):Expr<D> 
     return ECall('MIN', cast [e]);
 
+  public static function trim<D,O>(e:Field<D,O>):Expr<String>
+    return ECall('TRIM', cast [e]);
+
   public static function stContains<T>(g1:Expr<Geometry>, g2:Expr<Geometry>):Expr<Bool>
     return ECall('ST_Contains', cast [g1, g2]);
 
