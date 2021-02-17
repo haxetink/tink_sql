81b7be2cf4d43995530cccc65f3ff71236428355
Kevin Leung <kevinresol@gmail.com>
2021-02-17 13:26:10 +0800

    Workaround unification issue

--- src/tink/sql/Table.hx
+++ src/tink/sql/Table.hx
@@ -77,7 +77,7 @@ class TableSource<Fields, Filter:(Fields->Condition), Row:{}, Db>
   }
   
   public function insertMany(rows:Array<Insert<Row>>, ?options): Promise<Id<Row>>
-    return if (rows.length == 0) Promise.NULL
+    return if (rows.length == 0) cast Promise.NULL
       else cnx.execute(Insert({
         table: this, 
         rows: rows, 
