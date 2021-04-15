0da748d3ea88fd0ae209192e2cc449d6481a0449
Andy Li <andy@onthewings.net>
2021-04-15 10:55:13 +0800

    implement selection (not thoroughly tested)

--- src/tink/sql/drivers/node/PostgreSql.hx
+++ src/tink/sql/drivers/node/PostgreSql.hx
@@ -1,5 +1,6 @@
 package tink.sql.drivers.node;
 
+import haxe.Constraints.Function;
 import js.node.stream.Readable.Readable;
 import js.node.events.EventEmitter;
 import js.node.Buffer;
@@ -54,6 +55,7 @@ class PostgreSqlConnection<Db:DatabaseInfo> implements Connection<Db> implements
   var db:Db;
   var formatter:PostgreSqlFormatter;
   var parser:ResultParser<Db>;
+  var streamBatch:Int = 50;
 
   public function new(db, pool) {
     this.db = db;
@@ -84,6 +86,9 @@ class PostgreSqlConnection<Db:DatabaseInfo> implements Connection<Db> implements
       .query(queryOptions(query))
       .toPromise();
     return switch query {
+      case Select(_) | Union(_):
+        var parse:DynamicAccess<Any>->{} = parser.queryParser(query, formatter.isNested(query));
+        stream(queryOptions(query)).map(parse);
       case CreateTable(_, _) | DropTable(_) | AlterTable(_, _):
         fetch().next(function(r) {
           return Noise;
@@ -99,6 +104,14 @@ class PostgreSqlConnection<Db:DatabaseInfo> implements Connection<Db> implements
     trace(sql);
     #end
     return {text: sql};
+  }
+
+  function stream<T>(options: QueryOptions):Stream<T, Error> {
+    return Future.irreversible(resolve -> {
+      pool.query(options)
+        .then(r -> resolve(Success(Stream.ofIterator(r.rows.iterator()))))
+        .catchError(err -> resolve(Failure(err)));
+    });
   }
 }
 
@@ -132,11 +145,17 @@ private typedef QueryOptions = {
   ?types:Dynamic,
 }
 
+private typedef Submittable = {
+  function submit(connection:Dynamic):Void;
+}
+
+// https://node-postgres.com/api/pool
 @:jsRequire("pg", "Pool")
 private extern class Pool extends EventEmitter<Pool> {
   public function new(?config:PoolConfig):Void;
   public function connect():JsPromise<Client>;
   @:overload(function(config:QueryOptions):JsPromise<Result>{})
+  @:overload(function<S:Submittable>(s:S):S{})
   public function query(sql:String, ?values:Dynamic):JsPromise<Result>;
   public function end():JsPromise<Void>;
   public var totalCount:Int;
@@ -144,11 +163,13 @@ private extern class Pool extends EventEmitter<Pool> {
   public var waitingCount:Int;
 }
 
+// https://node-postgres.com/api/client
 @:jsRequire("pg", "Client")
 private extern class Client extends EventEmitter<Client> {
   public function new(?config:ClientConfig):Void;
   public function connect():JsPromise<Void>;
   @:overload(function(config:QueryOptions):JsPromise<Result>{})
+  @:overload(function<S:Submittable>(s:S):S{})
   public function query(sql:String, ?values:Dynamic):JsPromise<Result>;
   public function end():JsPromise<Void>;
   public function release(?err:Dynamic):JsPromise<Dynamic>;
@@ -159,6 +180,7 @@ private extern class Client extends EventEmitter<Client> {
   inline static public function escapeLiteral(str:String):String return untyped Client.prototype.escapeLiteral(str);
 }
 
+// https://node-postgres.com/api/result
 @:jsRequire("pg", "Result")
 private extern class Result {
   public var rows:Array<Dynamic>;
@@ -168,3 +190,17 @@ private extern class Result {
   public var command:String;
   public var rowCount:Int;
 }
+
+#if false // not used
+// https://node-postgres.com/api/cursor
+@:jsRequire("pg-cursor")
+private extern class Cursor extends EventEmitter<Cursor> {
+  public function new(text:String, values:Dynamic, ?config:{
+    ?rowMode:String,
+    ?types:Dynamic,
+  }):Void;
+  public function read(rowCount:Int, callback:JsError->Array<Dynamic>->Result->Void):Void;
+  public function close(?cb:?JsError->Void):Void;
+  public function submit(connection:Dynamic):Void;
+}
+#end
