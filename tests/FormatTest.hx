47d1a7686b620f8f00dbe64b9a0e70f099afe9eb
Kevin Leung <kevinresol@gmail.com>
2021-04-19 16:41:40 +0800

    Some efforts to seperate Info from table and database instances

--- tests/FormatTest.hx
+++ tests/FormatTest.hx
@@ -25,9 +25,9 @@ class FormatTest extends TestWithDb {
 	}
 
 	@:variant(new FormatTest.FakeTable1(), 'CREATE TABLE `fake` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `username` VARCHAR(50) NOT NULL, `admin` TINYINT NOT NULL, `age` INT UNSIGNED NULL)')
-	@:variant(this.db.User, 'CREATE TABLE `User` (`email` VARCHAR(50) NOT NULL, `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `location` VARCHAR(32) NULL, `name` VARCHAR(50) NOT NULL, PRIMARY KEY (`id`))')
-	@:variant(this.db.Types, 'CREATE TABLE `Types` (`abstractBool` TINYINT NULL, `abstractDate` DATETIME NULL, `abstractFloat` DOUBLE NULL, `abstractInt` INT NULL, `abstractString` VARCHAR(255) NULL, `blob` BLOB NOT NULL, `boolFalse` TINYINT NOT NULL, `boolTrue` TINYINT NOT NULL, `date` DATETIME NOT NULL, `enumAbstractBool` TINYINT NULL, `enumAbstractFloat` DOUBLE NULL, `enumAbstractInt` INT NULL, `enumAbstractString` VARCHAR(255) NULL, `float` DOUBLE NOT NULL, `int` INT NOT NULL, `nullBlob` BLOB NULL, `nullBool` TINYINT NULL, `nullDate` DATETIME NULL, `nullInt` INT NULL, `nullText` VARCHAR(40) NULL, `nullVarbinary` VARBINARY(10000) NULL, `optionalBlob` BLOB NULL, `optionalBool` TINYINT NULL, `optionalDate` DATETIME NULL, `optionalInt` INT NULL, `optionalText` VARCHAR(40) NULL, `optionalVarbinary` VARBINARY(10000) NULL, `text` VARCHAR(40) NOT NULL, `varbinary` VARBINARY(10000) NOT NULL)')
-	@:variant(this.uniqueDb.UniqueTable, 'CREATE TABLE `UniqueTable` (`u1` VARCHAR(123) NOT NULL, `u2` VARCHAR(123) NOT NULL, `u3` VARCHAR(123) NOT NULL, UNIQUE KEY `u1` (`u1`), UNIQUE KEY `index_name1` (`u2`, `u3`))')
+	@:variant(this.db.User.info, 'CREATE TABLE `User` (`email` VARCHAR(50) NOT NULL, `id` INT UNSIGNED NOT NULL AUTO_INCREMENT, `location` VARCHAR(32) NULL, `name` VARCHAR(50) NOT NULL, PRIMARY KEY (`id`))')
+	@:variant(this.db.Types.info, 'CREATE TABLE `Types` (`abstractBool` TINYINT NULL, `abstractDate` DATETIME NULL, `abstractFloat` DOUBLE NULL, `abstractInt` INT NULL, `abstractString` VARCHAR(255) NULL, `blob` BLOB NOT NULL, `boolFalse` TINYINT NOT NULL, `boolTrue` TINYINT NOT NULL, `date` DATETIME NOT NULL, `enumAbstractBool` TINYINT NULL, `enumAbstractFloat` DOUBLE NULL, `enumAbstractInt` INT NULL, `enumAbstractString` VARCHAR(255) NULL, `float` DOUBLE NOT NULL, `int` INT NOT NULL, `nullBlob` BLOB NULL, `nullBool` TINYINT NULL, `nullDate` DATETIME NULL, `nullInt` INT NULL, `nullText` VARCHAR(40) NULL, `nullVarbinary` VARBINARY(10000) NULL, `optionalBlob` BLOB NULL, `optionalBool` TINYINT NULL, `optionalDate` DATETIME NULL, `optionalInt` INT NULL, `optionalText` VARCHAR(40) NULL, `optionalVarbinary` VARBINARY(10000) NULL, `text` VARCHAR(40) NOT NULL, `varbinary` VARBINARY(10000) NOT NULL)')
+	@:variant(this.uniqueDb.UniqueTable.info, 'CREATE TABLE `UniqueTable` (`u1` VARCHAR(123) NOT NULL, `u2` VARCHAR(123) NOT NULL, `u3` VARCHAR(123) NOT NULL, UNIQUE KEY `u1` (`u1`), UNIQUE KEY `index_name1` (`u2`, `u3`))')
 	public function createTable(table:TableInfo, sql:String) {
 		// TODO: should separate out the sanitizer
 		return assert(formatter.createTable(table, false).toString(sanitizer) == sql);
@@ -37,7 +37,7 @@ class FormatTest extends TestWithDb {
 	@:variant(false, 'INSERT INTO `PostTags` (`post`, `tag`) VALUES (1, "haxe")')
 	public function insertIgnore(ignore, result) {
 		return assert(formatter.insert({
-			table: db.PostTags, 
+			table: db.PostTags.info, 
 			data: Literal([{post: 1, tag: 'haxe'}]),
 			ignore: ignore
 		}).toString(sanitizer) == result);
@@ -115,7 +115,7 @@ class FakeTable1 extends FakeTable {
 		];
 }
 
-class FakeTable implements TableInfo {
+class FakeTable implements  TableInfo {
 	public function new() {}
 
 	public function getName():String
