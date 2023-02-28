a77f60bcab7458f90541e268891619833d1307e1
Cédric Belin <cedric@belin.io>
2023-03-01 00:25:08 +0100

    Add the `NativeConfig` typedef [skip ci]

--- src/tink/sql/drivers/node/SqlServer.hx
+++ src/tink/sql/drivers/node/SqlServer.hx
@@ -1,5 +1,7 @@
 package tink.sql.drivers.node;
 
+import js.lib.Error;
+import js.lib.Promise;
 import tink.sql.Info.DatabaseInfo;
 
 class SqlServer implements Driver {
@@ -13,4 +15,47 @@ class SqlServer implements Driver {
 
   public function open<Db>(name: String, info: DatabaseInfo): Connection.ConnectionPool<Db>
     return null; // TODO
+}
+
+private typedef NativeConfig = {
+  final ?arrayRowMode: Bool;
+  final ?connectionTimeout: Int;
+  final ?database: String;
+  final ?domain: String;
+  final ?options: NativeDriverOptions;
+  final ?parseJSON: Bool;
+  final ?password: String;
+  final ?pool: NativeConnectionPoolOptions;
+  final ?port: Int;
+  final ?requestTimeout: Int;
+  final ?server: String;
+  final ?stream: Bool;
+  final ?user: String;
+}
+
+private typedef NativeConnectionPoolOptions = {
+  ?max: Int,
+  ?min: Int,
+  ?idleTimeoutMillis: Int
+}
+
+private typedef NativeDriverOptions = {
+  ?abortTransactionOnError: Bool,
+  ?appName: String,
+  ?encrypt: Bool,
+  ?instanceName: String,
+  ?useUTC: Bool,
+  ?tdsVersion: String,
+  ?trustServerCertificate: Bool
+}
+
+private extern class NativeConnectionPool {
+  function new(config: NativeConfig);
+  function connect(?callback: ?Error -> Void): Void;
+  function close(): Void;
+}
+
+@:jsRequire("msql")
+private extern class NativeDriver {
+  static function connect(config: NativeConfig): Promise<NativeConnectionPool>;
 }
